895b46fefada3d2ddd4c558cae346836ef226586
michaelpappas <pappas.michael@gmail.com>
2023-01-19 15:01:11 -0800

    partial work on editableTodo

--- src/EditableTodo.js
+++ src/EditableTodo.js
@@ -12,10 +12,12 @@ import TodoForm from "./TodoForm";
  * EditableTodoList -> EditableTodo -> { Todo, TodoForm }
  */
 
-function EditableTodo() {
-
+function EditableTodo({ remove, update }) {
+  const [isEditing, setIsEditing] = useState(false);
   /** Toggle if this is being edited */
-  function toggleEdit() { }
+  function toggleEdit() {
+    setIsEditing(!isEditing);
+  }
 
   /** Call remove fn passed to this. */
   function handleDelete() { }
@@ -24,31 +26,31 @@ function EditableTodo() {
   function handleSave(formData) { }
 
   return (
-      <div className="EditableTodo">
+    <div className="EditableTodo">
 
-                EITHER
+      EITHER
 
-                <TodoForm />
+      <TodoForm />
 
-                OR
+      OR
 
-                <div className="mb-3">
-                  <div className="float-end text-sm-end">
-                    <button
-                        className="EditableTodo-toggle btn-link btn btn-sm"
-                        onClick={toggleEdit}>
-                      Edit
-                    </button>
-                    <button
-                        className="EditableTodo-delBtn btn-link btn btn-sm text-danger"
-                        onClick={handleDelete}>
-                      Del
-                    </button>
-                  </div>
-                  <Todo />
-                </div>
-
+      <div className="mb-3">
+        <div className="float-end text-sm-end">
+          <button
+            className="EditableTodo-toggle btn-link btn btn-sm"
+            onClick={toggleEdit}>
+            Edit
+          </button>
+          <button
+            className="EditableTodo-delBtn btn-link btn btn-sm text-danger"
+            onClick={handleDelete}>
+            Del
+          </button>
+        </div>
+        <Todo />
       </div>
+
+    </div>
   );
 }
 
